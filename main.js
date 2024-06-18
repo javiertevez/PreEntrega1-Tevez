be7baa6c2f06782e84561c8e849b71a8892f6955
Javier Tevez <javier.tevez.dev@gmail.com>
2024-06-17 22:40:46 -0300

    Mejora en la opción de dejar de comprar

--- main.js
+++ main.js
@@ -7,168 +7,288 @@ while (contraseña !== 'coderhouse') {
 
 alert('Bienvenido!');
 
-comprar ();
+comprar();
 
-function comprar () {
-   let quiere = prompt('Por favor, ingrese qué tipo de producto quiere comprar marcando la letra según corresponda: a) Zapatillas b) Buzos c) Remeras');
+function comprar() {
+    let quiere = prompt('Por favor, ingrese qué tipo de producto quiere comprar marcando la letra según corresponda: a) Zapatillas b) Buzos c) Remeras');
 
 
-   switch (quiere) {
-   //Si el usuario elije a) zapatillas
-    case 'a':
-        let zapa = prompt ('Elija qué zapatillas desea: a) Nike Airforce blancas b) Puma SuedeXL azules c) Adidas Campus verdes d) DC Pure negras')
-        switch (zapa) {
-            case 'a':
-                let nike = prompt('¿Quiere realizar la compra de las Nike Airforce blancas?. Responda por "si" o "no"');
-                if (nike === 'si') {
-                    alert('Muchas gracias por su compra!');
-                    return comprar ();
-                } else {
-                    alert('Compra cancelada')
-                    return comprar ();
-                }
-                break;
+    switch (quiere) {
+        //Si el usuario elije a) zapatillas
+        case 'a':
+            let zapa = prompt('Elija qué zapatillas desea: a) Nike Airforce blancas b) Puma SuedeXL azules c) Adidas Campus verdes d) DC Pure negras')
+            switch (zapa) {
+                case 'a':
+                    let nike = prompt('¿Quiere realizar la compra de las Nike Airforce blancas?. Responda por "si" o "no"');
+                    if (nike === 'si') {
+                        alert('Muchas gracias por su compra!');
+                        respuesta();
+                        function respuesta() {
+                            let respuesta = prompt('¿Desea comprar algo más? Responda por "si" o "no".');
+                            if (respuesta === 'si') {
+                                return comprar();
+                            } else if (respuesta === 'no') {
+                                alert ('Hasta luego!');
+                                return;
+                            } else {
+                                alert('Responda por "si" o por "no".');
+                                return respuesta();
+                            }
+                        }
+                    } else {
+                        alert('Compra cancelada')
+                        return comprar();
+                    }
+                    break;
 
                 case 'b':
                     let puma = prompt('¿Quiere realizar la compra de las Puma SuedeXL azules? Responda por "si" o "no"');
                     if (puma === 'si') {
                         alert('Muchas gracias por su compra!');
+                        respuesta ();
+                    function respuesta () {
+                    let respuesta = prompt('¿Desea comprar algo más? Responda por "si" o "no".');
+                    if (respuesta === 'si') {
                         return comprar ();
+                    } else if (respuesta === 'no') {
+                        alert ('Hasta luego!');
+                        return;
+                    } else {
+                        alert ('Responda por "si" o por "no".');
+return respuesta ();
+                    }
+                }
                     } else {
                         alert('Compra cancelada')
-                        return comprar ();
+                        return comprar();
                     }
                     break;
 
-                    case 'c':
-                        let adidas = prompt('¿Quiere realizar la compra de las Adidas Campus verdes? Responda por "si" o "no"');
-                        if (adidas === 'si') {
-                            alert('Muchas gracias por su compra!');
-                            return comprar ();
-                        } else {
-                            alert('Compra cancelada');
-                            return comprar ();
-                        }
-                        break;
-            
-                        case 'd':
-                            let dc = prompt('¿Quiere realizar la compra de las DC Pure negras? Responda por "si" o "no"');
-                            if (dc === 'si') {
-                                alert('Muchas gracias por su compra!');
-                           return comprar ();
-                            } else {
-                                alert ('Compra cancelada');
-                                return comprar ();
-                            }
-                            break;
-        
-            default:
-                alert('Error, por favor ingrese alguna de las opciones disponibles (a, b, c y d).');
-                comprar ();
-                break;
+                case 'c':
+                    let adidas = prompt('¿Quiere realizar la compra de las Adidas Campus verdes? Responda por "si" o "no"');
+                    if (adidas === 'si') {
+                        alert('Muchas gracias por su compra!');
+                        respuesta ();
+                    function respuesta () {
+                    let respuesta = prompt('¿Desea comprar algo más? Responda por "si" o "no".');
+                    if (respuesta === 'si') {
+                        return comprar ();
+                    } else if (respuesta === 'no') {
+                    alert ('Hasta luego!');
+                    return;
+                    } else {
+                        alert ('Responda por "si" o por "no".');
+return respuesta ();
+                    }
+                }
+                    } else {
+                        alert('Compra cancelada');
+                        return comprar();
+                    }
+                    break;
 
-                
-        }
+                case 'd':
+                    let dc = prompt('¿Quiere realizar la compra de las DC Pure negras? Responda por "si" o "no"');
+                    if (dc === 'si') {
+                        alert('Muchas gracias por su compra!');
+                        respuesta();
+                        function respuesta() {
+                            let respuesta = prompt('¿Desea comprar algo más? Responda por "si" o "no".');
+                            if (respuesta === 'si') {
+                                return comprar();
+                            } else if (respuesta === 'no') {
+                                alert ('Hasta luego!');
+                                return;
+                            } else {
+                                alert('Responda por "si" o por "no".');
+                                return respuesta();
+                            }
+                        }
+                    } else {
+                        alert('Compra cancelada');
+                        return comprar();
+                    }
+                    break;
+
+                default:
+                    alert('Error, por favor ingrese alguna de las opciones disponibles (a, b, c y d).');
+                    comprar();
+                    break;
+
+
+            }
 
         //Si el usuario elije b) buzos
-case 'b':
-        let buzo = prompt ('Elija qué buzo desea: a) Adidas b) Nike c) Topper');
-        switch (buzo) {
-            case 'a':
-                let adidasb = prompt('¿Quiere realizar la compra del buzo Adidas?. Responda por "si" o "no"');
-                if (adidasb === 'si') {
-                    alert('Muchas gracias por su compra!');
-                    return comprar ();
-                } else {
-                    alert('Compra cancelada')
-                    return comprar ();
-                }
-                break;
+        case 'b':
+            let buzo = prompt('Elija qué buzo desea: a) Adidas b) Nike c) Topper');
+            switch (buzo) {
+                case 'a':
+                    let adidasb = prompt('¿Quiere realizar la compra del buzo Adidas?. Responda por "si" o "no"');
+                    if (adidasb === 'si') {
+                        alert('Muchas gracias por su compra!');
+                        respuesta();
+                        function respuesta() {
+                            let respuesta = prompt('¿Desea comprar algo más? Responda por "si" o "no".');
+                            if (respuesta === 'si') {
+                                return comprar();
+                            } else if (respuesta === 'no') {
+                                alert ('Hasta luego!');
+                                return;
+                            } else {
+                                alert('Responda por "si" o por "no".');
+                                return respuesta();
+                            }
+                        }
+                    } else {
+                        alert('Compra cancelada')
+                        return comprar();
+                    }
+                    break;
 
                 case 'b':
                     let puma = prompt('¿Quiere realizar la compra del buzo Nike? Responda por "si" o "no"');
                     if (puma === 'si') {
                         alert('Muchas gracias por su compra!');
-                        return comprar ();
+                        respuesta();
+                        function respuesta() {
+                            let respuesta = prompt('¿Desea comprar algo más? Responda por "si" o "no".');
+                            if (respuesta === 'si') {
+                                return comprar();
+                            } else if (respuesta === 'no') {
+                                alert ('Hasta luego!');
+                                return;
+                            } else {
+                                alert('Responda por "si" o por "no".');
+                                return respuesta();
+                            }
+                        }
                     } else {
                         alert('Compra cancelada')
-                        return comprar ();
+                        return comprar();
                     }
                     break;
 
-                    case 'c':
-                        let adidas = prompt('¿Quiere realizar la compra del buzo Topper? Responda por "si" o "no"');
-                        if (adidas === 'si') {
-                            alert('Muchas gracias por su compra!');
-                            return comprar ();
-                        } else {
-                            alert('Compra cancelada');
-                            return comprar ();
+                case 'c':
+                    let adidas = prompt('¿Quiere realizar la compra del buzo Topper? Responda por "si" o "no"');
+                    if (adidas === 'si') {
+                        alert('Muchas gracias por su compra!');
+                        respuesta();
+                        function respuesta() {
+                            let respuesta = prompt('¿Desea comprar algo más? Responda por "si" o "no".');
+                            if (respuesta === 'si') {
+                                return comprar();
+                            } else if (respuesta === 'no') {
+                                alert ('Hasta luego!');
+                                return;
+                            } else {
+                                alert('Responda por "si" o por "no".');
+                                return respuesta();
+                            }
                         }
-                        break;
-            
-                
-        
-            default:
-                alert('Error, por favor ingrese alguna de las opciones disponibles (a, b, c y d).');
-                comprar ();
-                break;
+                    } else {
+                        alert('Compra cancelada');
+                        return comprar();
+                    }
+                    break;
 
-                
-        }
+
+
+                default:
+                    alert('Error, por favor ingrese alguna de las opciones disponibles (a, b, c y d).');
+                    comprar();
+                    break;
+
+
+            }
 
         //Si el usuario elije c) remeras
 
         case 'c':
-        let remera = prompt ('Elija qué remera desea: a) Blanca b) Negra c) Beige');
-        switch (remera) {
-            case 'a':
-                let adidasb = prompt('¿Quiere realizar la compra de la remera negra?. Responda por "si" o "no"');
-                if (adidasb === 'si') {
-                    alert('Muchas gracias por su compra!');
-                    return comprar ();
-                } else {
-                    alert('Compra cancelada')
-                    return comprar ();
-                }
-                break;
+            let remera = prompt('Elija qué remera desea: a) Blanca b) Negra c) Beige');
+            switch (remera) {
+                case 'a':
+                    let adidasb = prompt('¿Quiere realizar la compra de la remera negra?. Responda por "si" o "no"');
+                    if (adidasb === 'si') {
+                        alert('Muchas gracias por su compra!');
+                        respuesta();
+                        function respuesta() {
+                            let respuesta = prompt('¿Desea comprar algo más? Responda por "si" o "no".');
+                            if (respuesta === 'si') {
+                                return comprar();
+                            } else if (respuesta === 'no') {
+                                alert ('Hasta luego!');
+                                return;
+                            } else {
+                                alert('Responda por "si" o por "no".');
+                                return respuesta();
+                            }
+                        }
+                    } else {
+                        alert('Compra cancelada')
+                        return comprar();
+                    }
+                    break;
 
                 case 'b':
                     let puma = prompt('¿Quiere realizar la compra de la remera blanca? Responda por "si" o "no"');
                     if (puma === 'si') {
                         alert('Muchas gracias por su compra!');
-                        return comprar ();
+                        respuesta();
+                        function respuesta() {
+                            let respuesta = prompt('¿Desea comprar algo más? Responda por "si" o "no".');
+                            if (respuesta === 'si') {
+                                return comprar();
+                            } else if (respuesta === 'no') {
+                                alert ('Hasta luego!');
+                                return;
+                            } else {
+                                alert('Responda por "si" o por "no".');
+                                return respuesta();
+                            }
+                        }
                     } else {
                         alert('Compra cancelada')
-                        return comprar ();
+                        return comprar();
                     }
                     break;
 
-                    case 'c':
-                        let adidas = prompt('¿Quiere realizar la compra de la remera beige? Responda por "si" o "no"');
-                        if (adidas === 'si') {
-                            alert('Muchas gracias por su compra!');
-                            return comprar ();
-                        } else {
-                            alert('Compra cancelada');
-                            return comprar ();
+                case 'c':
+                    let adidas = prompt('¿Quiere realizar la compra de la remera beige? Responda por "si" o "no"');
+                    if (adidas === 'si') {
+                        alert('Muchas gracias por su compra!');
+                        respuesta();
+                        function respuesta() {
+                            let respuesta = prompt('¿Desea comprar algo más? Responda por "si" o "no".');
+                            if (respuesta === 'si') {
+                                return comprar();
+                            } else if (respuesta === 'no') {
+                                alert ('Hasta luego!');
+                                return;
+                            } else {
+                                alert('Responda por "si" o por "no".');
+                                return respuesta();
+                            }
                         }
-                        break;
-            
-                
-        
-            default:
-                alert('Error, por favor ingrese alguna de las opciones disponibles (a, b, c y d).');
-                comprar ();
-                break;
-
-                
-        }
+                    } else {
+                        alert('Compra cancelada');
+                        return comprar();
+                    }
+                    break;
 
 
-        break;
-       
-    default:
-        break;
-}
+
+                default:
+                    alert('Error, por favor ingrese alguna de las opciones disponibles (a, b, c y d).');
+                    comprar();
+                    break;
+
+
+            }
+
+
+            break;
+
+        default:
+            break;
+    }
 }
